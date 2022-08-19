fa8a3ae6d8af44ac396f6a2894e245437c385768
l050101 <l01062506145@gmail.com>
2022-08-20 03:15:37 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const something = () =>{
+    const invoke = window.__TAURI__.invoke
+
+    // 이제 명령을 호출할 수 있습니다!
+    // 앱 배경을 우클릭하고 개발자 도구를 엽니다.
+    // "Hello, World!"가 콘솔에 출력되는 걸 볼 수 있습니다!
+    invoke('greet', { name: 'World' })
+      // `invoke`는 Promise를 반환합니다
+      .then((response) => console.log(response))
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={something}>hellllo</button>
         <a
           className="App-link"
           href="https://reactjs.org"
